2861128561fdb7fcc1762cee1b3007994ce77345
rbaum <baumraphael@gmail.com>
2014-11-17 19:30:04 +0100

    journey to understand

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,12 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/15 19:55:05 by rbaum             #+#    #+#             */
-/*   Updated: 2014/11/16 19:53:55 by rbaum            ###   ########.fr       */
+/*   Updated: 2014/11/17 19:29:36 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
-
-/*
- Le premier paramètre est le descripteur de fichier depuis lequel lire.
-• Le second paramètre est l’adresse d’un pointeur sur caractère qui servira à stocker
-la ligne lue sur le descripteur de fichier.
-• la valeur de retour peut être 1, 0 ou -1 selon qu’une ligne a été lue, que la lecture est terminée ou bien qu’une erreur est survenue respectivement.
-• Votre fonction get_next_line doit renvoyer son resultat sans le ’\n’.
-• Un appel en boucle à votre fonction get_next_line permettra donc de lire le texte
-disponible sur un descripteur de fichier une ligne à la fois jusqu’à la fin du texte.
-• Assurez-vous que votre fonction se comporte bien lorsqu’elle lit depuis un fichier, depuis l’entrée standard, depuis une redirection, etc.*/
 
 char		*avant(char *s)
 {
@@ -56,47 +46,18 @@ int			get_next_line(int const fd, char **line)
 	static char *save;
 	char		buff[BUFF_SIZE + 1];
 	int			ret;
-
-	ret = read(fd, buff, BUFF_SIZE);
+//Buff contient ce aue jai lu sur buff size) Read dans une boucle
+	if(!(ret = read(fd, buff, BUFF_SIZE)))
+		return (-1);
+	if (buff == avant(*line))
+		return (1);
+	if (!(save == apres(*line)))
+		return (0);
 	buff[ret] = '\0';
-	
 
 }
-
-
-int		main(int argc, char **argv)
-{
-	int fd;
-	int ret;
-	char *line;
-
-	if (argc != 2)
-		ft_putstr("Error: File name missing!\n");
-	else
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-			ft_putstr("Error: Can't open file!\n");
-		else
-		{
-			while((ret = get_next_line(fd, &line)) != 0)
-			{
-				ft_putstr(line);
-				ft_putstr("\n");
-			}
-			ft_putstr(line);
-			ft_putstr("\n");
-			return (close(fd));
-		}
-	}
-	return (0);
-}
-
-
-
-
-
-  /*static char     *next_line(char *str)
+*/
+static char     *next_line(char *str)
 {
     char        *n_str;
     int         i;
@@ -127,4 +88,59 @@ static char     *ft_read_line(char *str, char c)
     line[i] = '\0';
     return (line);
 }
-  */
+
+int             get_next_line(int const fd, char **line)
+{
+    int         ret;
+    char        buff[BUFF_SIZE + 1];
+    static char *save;
+
+    if (BUFF_SIZE <= 0 || line == NULL)
+        return (-1);
+    if (save == NULL)
+        save = ft_strnew(1);
+    ret = 2;
+    while (ft_strchr(save, '\n') == NULL && ret != 0)
+    {
+        ret = read(fd, buff, BUFF_SIZE);
+        if (ret == -1)
+            return (-1);
+        buff[ret] = '\0';
+        save = ft_strjoin(save, buff);
+    }
+    *line = (ret > 0) ? ft_read_line(save, '\n') : ft_read_line(save, '\0');
+    if (ret == 0)
+        return (0);
+    save = next_line(save);
+    return (1);
+}
+/*
+int		main(int argc, char **argv)
+{
+	int fd;
+	int ret;
+	char *line;
+
+	if (argc != 2)
+		ft_putstr("Error: File name missing!\n");
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			ft_putstr("Error: Can't open file!\n");
+		else
+		{
+			while((ret = get_next_line(fd, &line)) > 0)
+			{
+				ft_putstr(line);
+				ft_putstr("\n");
+			}
+			ft_putstr(line);
+			ft_putstr("\n");
+			return (close(fd));
+		}
+	}
+	return (0);
+} 
+*/
+ 
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/15 20:29:50 by rbaum             #+#    #+#             */
-/*   Updated: 2014/11/16 19:55:04 by rbaum            ###   ########.fr       */
+/*   Updated: 2014/11/17 17:12:06 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 0
 
 # include <sys/types.h>
 # include <sys/uio.h>
